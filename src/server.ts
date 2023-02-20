bfd79c14c95284d8433620ebf1271158bf66c651
Almir R <almir@iamalmir.tech>
2023-02-20 13:32:45 -0500

    chalk

--- src/server.ts
+++ src/server.ts
@@ -26,5 +26,5 @@ app.use(HomeRoutes.routes())
 // Run the server
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
