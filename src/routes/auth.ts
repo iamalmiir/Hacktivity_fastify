973af54a12da83d09f4a132efa26a3b282d470e5
Almir Redzematovic <almir@iamalmir.tech>
2023-02-21 14:33:33 -0500

    - Documented routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,35 +3,45 @@ import Router from 'koa-router'
 
 const _ = new Router()
 
+// Login route for local strategy
 _.post(
   '/auth/local/login',
   passport.authenticate('local', {
+    // If authentication failed redirect to /failed/auth
     failureRedirect: '/failed/auth',
   }),
   async (ctx, next) => {
-    if (ctx.isAuthenticated()) {
-      ctx.body = {
-        success: true,
-        message: 'Successfully logged in',
-      }
+    // If authentication succeeded send success message to the client
+    ctx.body = {
+      success: true,
+      message: 'Successfully logged in',
     }
 
     await next()
   }
 )
 
+// Logout route
 _.get('/logout', async (ctx, next) => {
+  // If user is authenticated log them out and send success message to the client
+  // Otherwise send failed message to the client
   if (ctx.isAuthenticated()) {
     ctx.logout()
     ctx.body = {
       success: true,
       message: 'Successfully logged out',
     }
+  } else {
+    ctx.body = {
+      success: false,
+      message: 'Failed to logout',
+    }
   }
 
   await next()
 })
 
+// Failed authentication route
 _.get('/failed/auth', async (ctx, next) => {
   ctx.body = {
     success: false,
