aaf4ceeb9e0189b25a718f7f2c8b98f8827c16e9
Almir Redzematovic <almir@iamalmir.tech>
2023-02-23 23:47:47 -0500

    logic improvements

--- src/config/localAuthStrategy.ts
+++ src/config/localAuthStrategy.ts
@@ -3,56 +3,66 @@ import prisma from '@common/db'
 import { Strategy as LocalStrategy } from 'passport-local'
 import { comparePassword } from '@common/encrypt'
 
+// Serialize user
 passport.serializeUser((user: any, done) => {
   done(null, user.id)
 })
 
+// Deserialize user
 passport.deserializeUser(async (id: string, done) => {
   try {
+    // Find user in database
     const user = await prisma.user.findUnique({
       where: {
         id,
       },
     })
 
+    // If user not found, return false
     if (!user) {
       return done(null, false)
     }
-
+    // If user found, return user
     return done(null, user)
   } catch (error) {
     return done(error)
   }
 })
 
+// Local Strategy
 passport.use(
   new LocalStrategy(
     {
+      // Set username field to email
       usernameField: 'email',
       passwordField: 'password',
     },
     async (email, password, done) => {
       try {
+        // Find user in database by email
         const user = await prisma.user.findUnique({
           where: {
             email,
           },
         })
 
+        // If user not found, return false along with error message
         if (!user) {
           return done(null, false, {
             message: "Uh oh, that did't work! Please check your info.",
           })
         }
-
+        // If user found, compare password with hashed password
         if (!(await comparePassword(password, user.password))) {
           return done(null, false, {
             message: "Uh oh, that did't work! Please check your info.",
           })
         }
 
+        // If password matches, return user
         return done(null, user)
       } catch (error) {
+        // If error, return error
         return done(error, false, {
           message: "Uh oh, that did't work! Please check your info.",
         })
